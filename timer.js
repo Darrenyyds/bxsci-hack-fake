42805fccf9c41f13ba90c598c57fdc7ca3bb556c
Darrenyyds <121652033+Darrenyyds@users.noreply.github.com>
2023-03-21 22:52:17 -0400

    Add files via upload

---
+++ timer.js
@@ -0,0 +1,21 @@
+/** @format */
+
+var countDownDate = new Date().getTime() + 60 * 30000;
+var x = setInterval(function () {
+  // Find the distance between now and the count down date
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
